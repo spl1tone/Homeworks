5daa77fb695f90ba2ec06b45792675f0c0f90567
spl1tOne <levonvlad144@gmail.com>
2023-06-27 20:20:36 +0300

    1

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -6,70 +6,36 @@
 
         static void Main (string[] args)
         {
-            Console.WriteLine($"MaxValue: {MaxValue(5,10)}");
-            Console.WriteLine($"MinValue: {MinValue(5,10)}");
-            TrySumIfOdd(5, 10, out int sum);
-            Console.WriteLine($"Repeat SomeText: {Repeat("str",3)}");
-        }
+            int iRef = 0; 
+            var someOut = 5;
 
-        static int MaxValue (int num1, int num2)
-        {
-            int value = Math.Max (num1, num2);
-            return value;
-        }
-        static int MaxValue (int num1, int num2,bool degree,int degreeNum)
-        {           
-            int value = Math.Max(num1, num2);
-            if (degree) value = (int)Math.Pow(value, degreeNum);
-            return value;
-        }
-        static int MinValue (int num1, int num2)
-        {
-            int value = Math.Min(num1, num2);
-            return value;
-        }
-        static int MinValue (int num1, int num2, bool degree, int degreeNum)
-        {
-            int value = Math.Min(num1, num2);
-            if (degree) value = (int)Math.Pow(value, degreeNum);
-            return value;
-        }
+            PrintValue(5,"Vlad",out someOut,ref iRef,5);
 
-        static void TrySumIfOdd (int num1, int num2, out int sum)
-        {
-            sum = 0;
-            if (num1 < num2) {
-                for(int i = num1; i < num2; i++) {
-                    sum += i;
-                }
-            }
-            else if (num1 > num2) {
-                for (int i = num2; i < num1; i++) {
-                    sum += i;
-                }
-            }
-            else {
-                sum = num1;          
-            }
-
-            if (sum % 2 == 0) {
-                Console.WriteLine($"{sum} - Це число парне");
-            }
-            else {
-                Console.WriteLine($"{sum} - Це число непарне");
-            }
-        }
-
-        static string Repeat(string text,int count)
-        {
-            string rText = string.Empty;
-            for(int i = 0; i < count; i++) {
-                rText += text;
-            }
-            return rText;
+            Console.WriteLine(someOut);
+            Console.WriteLine(iRef);
+            Console.WriteLine(SomeMethod("Name",new string[] {"Vlad","Misha","Nikita"}));
         }
         
+        static string SomeMethod(string title, params string[] words)
+        {
+            string head = title + ": ";
 
-        
+            foreach(var word in words) {
+                head += word + ", ";
+            }
+
+            return head;
+        }
+
+
+        static void PrintValue (int val, string str, out int res, ref int someValue, int opt = 1)
+        {
+            someValue = someValue + 1;
+            val = someValue + opt + 5;
+            res = opt + 1;
+            Console.WriteLine($"{val},{str},{someValue},{res},{opt}");
+
+        }
+
     }
 }
