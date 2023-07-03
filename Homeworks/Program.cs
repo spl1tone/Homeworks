9db8af735bdc52c18b2ca6b04a5ef9b7eaa21fb9
spl1tOne <levonvlad144@gmail.com>
2023-07-03 22:44:29 +0300

    Analyze + Sort

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -6,10 +6,15 @@
 
         static void Main (string[] args)
         {
-           bool isCompare = Compare("Test", "Tast");
-           Console.WriteLine(isCompare); // False
+            bool isCompare = Compare("Test", "Tast");
+            Console.WriteLine($"IsCompare: {isCompare}"); // False
 
+            int alphabetics, digits, special = 0;
+            Analyze("Test1234$",out alphabetics,out digits, out special);
+            Console.WriteLine($"Alphabetics: {alphabetics} \nDigits: {digits} \nSpecial: {special}");
 
+            string sorted = Sort("Hello");
+            Console.WriteLine($"Sorted: {sorted}");
         }
 
         static bool Compare (string str1, string str2)
@@ -21,6 +26,31 @@
             }
 
             return true;
+        }
+
+        static void Analyze(string str, out int alphabetic, out int digits, out int special)
+        {
+            alphabetic = 0; 
+            digits = 0; 
+            special = 0; 
+
+            for(int i = 0; i < str.Length; i++) {
+                if (char.IsLetter(str[i])) {
+                    alphabetic++;
+                }
+               else if (char.IsDigit(str[i])) {
+                    digits++;
+                }
+                else {
+                    special++;
+                }
+            }
+        }
+
+        static string Sort(string str)
+        {
+            var sorted = str.OrderBy(x => x);
+            return new string(sorted.ToArray());
         }
     }
 }
