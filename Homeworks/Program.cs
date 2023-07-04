2092b880acb1616d55036e62b66c4b620cbec80b
spl1tOne <levonvlad144@gmail.com>
2023-07-04 19:33:51 +0300

    2

--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -27,6 +27,26 @@ namespace Homeworks
             string replacment = "_";
             string result = Regex.Replace(input, pattern, replacment);
             Console.WriteLine("Replace {0}",result);
+
+            pattern = "fox";
+            Console.WriteLine(Regex.IsMatch(input,pattern)); // True
+
+            input = "+38(050)1234567";
+           string input2 = "+38(050)12345";
+           string input3 = "+38(050)1234";
+            pattern = @"\+38\(0\d{2}\)\d{5,7}"; // From 5 digits to 7
+            Console.WriteLine(Regex.IsMatch(input,pattern)); // True
+            Console.WriteLine(Regex.IsMatch(input2,pattern)); // True
+            Console.WriteLine(Regex.IsMatch(input3,pattern)); // False
+
+            string i1 = "Іваненко І.І.";
+            string i2 = "Іваненко Іван Іванович";
+
+            int firstI = (int)i1[0];
+            int secondI = (int)i1[1];
+
+            Console.WriteLine(firstI);
+            Console.WriteLine(secondI);
         }
         
     }
