053a18fbc712982d90ac4538b587afd963b78317
spl1tOne <levonvlad144@gmail.com>
2023-08-11 00:03:13 +0300

    test

---
+++ Homeworks/APIModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Homeworks;
+
+public class APIModel
+{
+    public string Quote { get; set; }
+    public string Author { get; set; }
+}
--- Homeworks/Program.cs
+++ Homeworks/Program.cs
@@ -1,75 +1,31 @@
-﻿namespace Homeworks
+﻿using Newtonsoft.Json;
+namespace Homeworks;
+
+internal class Program
 {
-    internal class Program
+    private static void Main ()
     {
-        // Homework lesson-5
-
-        static void Main (string[] args)
-        {
-            Console.WriteLine($"MaxValue: {MaxValue(5,10)}");
-            Console.WriteLine($"MinValue: {MinValue(5,10)}");
-            TrySumIfOdd(5, 10, out int sum);
-            Console.WriteLine($"Repeat SomeText: {Repeat("str",3)}");
-        }
-
-        static int MaxValue (int num1, int num2)
-        {
-            int value = Math.Max (num1, num2);
-            return value;
-        }
-        static int MaxValue (int num1, int num2,bool degree,int degreeNum)
-        {           
-            int value = Math.Max(num1, num2);
-            if (degree) value = (int)Math.Pow(value, degreeNum);
-            return value;
-        }
-        static int MinValue (int num1, int num2)
-        {
-            int value = Math.Min(num1, num2);
-            return value;
-        }
-        static int MinValue (int num1, int num2, bool degree, int degreeNum)
-        {
-            int value = Math.Min(num1, num2);
-            if (degree) value = (int)Math.Pow(value, degreeNum);
-            return value;
-        }
-
-        static void TrySumIfOdd (int num1, int num2, out int sum)
-        {
-            sum = 0;
-            if (num1 < num2) {
-                for(int i = num1; i < num2; i++) {
-                    sum += i;
-                }
-            }
-            else if (num1 > num2) {
-                for (int i = num2; i < num1; i++) {
-                    sum += i;
-                }
-            }
-            else {
-                sum = num1;          
-            }
-
-            if (sum % 2 == 0) {
-                Console.WriteLine($"{sum} - Це число парне");
-            }
-            else {
-                Console.WriteLine($"{sum} - Це число непарне");
-            }
-        }
-
-        static string Repeat(string text,int count)
-        {
-            string rText = string.Empty;
-            for(int i = 0; i < count; i++) {
-                rText += text;
-            }
-            return rText;
-        }
-        
-
-        
+        PrintAPIInfoAsync();
+        Console.ReadLine();
     }
+
+
+    private static async Task PrintAPIInfoAsync ()
+    {
+        using (HttpClient client = new HttpClient()) {
+            try {
+                HttpResponseMessage response = await client.GetAsync("https://api.breakingbadquotes.xyz/v1/quotes/100");
+
+                var quotesInfo = JsonConvert.DeserializeObject<APIModel[]>(await response.Content.ReadAsStringAsync());
+
+                for (var i = 0; i < quotesInfo.Length; i++) {
+                    Console.WriteLine($"Quote: {quotesInfo[i].Quote}\nAuthor: {quotesInfo[i].Author}\n" + new string('-', 30));
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+
 }
